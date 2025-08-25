7073618d7d924b9f5fd18cc4816bd2e6027473b9
Gabriel Santos <163359098+gabssiin@users.noreply.github.com>
2025-08-25 22:23:19 +0000

    adicionando midlewares cors

--- ex1/api/index.js
+++ ex1/api/index.js
@@ -1,14 +1,25 @@
 import "dotenv/config";
 import express from "express";
+import cors from 'cors';
 
 console.log("Iniciando servidor..");
 console.log("MY_SECRET", process.env.MY_SECRET);
 console.log("PYTHON_ROOT", process.env.MY_OTHER_SECRET);
 
 const app = express();
+app.use(cors());
+
+app.use((req,res,next)=>{
+  console.log(`${req.method} - ${req.path} - ${req.ip}`);
+    next();
+});
 
 app.get("/", (req, res) => {
   res.send("Bem vindo ao Express de Gabriel Santos!");
+});
+
+app.get("/example", (req,res)=>{
+  res.send("Bem vindo ao exemplo");
 });
 
 app.listen(3000, () => {

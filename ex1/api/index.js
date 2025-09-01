9480fc133d272999ad6219710915c56b8559698f
Gabriel Santos <163359098+gabssiin@users.noreply.github.com>
2025-09-01 22:59:56 +0000

    aula 01/09

--- ex1/api/index.js
+++ ex1/api/index.js
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import express from "express";
 import cors from 'cors';
+import { v4 as uuidv4 } from 'uuid';
+
 
 console.log("Iniciando servidor..");
 console.log("MY_SECRET", process.env.MY_SECRET);
@@ -8,6 +10,12 @@ console.log("PYTHON_ROOT", process.env.MY_OTHER_SECRET);
 
 const app = express();
 
+/* body-parset */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+/*
+const date = Date.parse(req.body.date);
+const count = Number(req.body.count);*/
 var corsOptions = {
   origin: ['http://example.com', '*'],
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -47,6 +55,8 @@ app.use((req, res, next) => {
   next();
 });
 
+
+/*GETS */
 app.get("/", (req, res) => {
   res.send("Bem vindo ao Express de Gabriel Santos!");
 });
@@ -66,4 +76,102 @@ app.get("/example", (req, res) => {
 app.listen(3000, () => {
   console.log("Servidor funcionando na porta: 3000");
   console.log("Acesse http://localhost:3000");
+});
+
+/*DATA -----------------------------*/
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
+/* ------------------------ USERS --------------------------------- */
+
+
+app.get('/users', (req, res) => {
+  return res.send(Object.values(users));
+});
+
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+
+app.put('/users', (req, res) => {
+  return res.send('PUT HTTP method on user resource');
+});
+
+app.delete('/users', (req, res) => {
+  return res.send('DELETE HTTP method on user resource');
+});
+
+
+app.get('/users', (req, res) => {
+  return res.send('GET HTTP method on user resource');
+});
+
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+
+/*END*/
+
+app.put('/users/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+app.delete('/users/:userId', (req, res) => {
+  return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource`,
+  );
+}); 
+
+
+//** uuid */
+app.post('/messages', (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
+});
+
+app.post('/messages', (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
+});
+
+
+app.use((req, res, next) => {
+  // do something
+  next();
 });
